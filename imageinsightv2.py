9ded3caca28001259ffe29e4f58c3e27353cea9b
subhangmokkarala <subhangmokkarala@gmail.com>
2023-12-25 00:54:08 +0530

    added v2 with resnet

---
+++ imageinsightv2.py
@@ -0,0 +1,66 @@
+from telegram.ext import Updater, Filters, CommandHandler, MessageHandler
+import cv2
+from tensorflow.keras.applications.resnet50 import ResNet50
+import numpy as np
+from labels import lbl
+import os
+from datetime import datetime
+
+model = ResNet50()
+
+def start(updater, context): 
+	updater.message.reply_text("Welcome to the classification bot!")
+
+def help_(updater, context): 
+	updater.message.reply_text("Just send the image you want to classify.")
+
+def message(updater, context):
+	msg = updater.message.text
+	print(msg)
+	updater.message.reply_text(msg)
+
+def image(updater, context):
+    # Create a folder to store images if it doesn't exist
+    if not os.path.exists("User_data"):
+        os.makedirs("User_data")
+
+    photo = updater.message.photo[-1].get_file()
+    photo_path = os.path.join("User_data", "img.jpg")  # Save the image in the folder
+    photo.download(photo_path)
+
+    img = cv2.imread(photo_path)
+
+    img = cv2.resize(img, (224, 224))
+    img = np.reshape(img, (1, 224, 224, 3))
+
+    pred = np.argmax(model.predict(img))
+
+    pred = lbl[pred]
+
+    print(pred)
+
+    updater.message.reply_text(pred)
+
+    # Move the saved image to a new unique filename with a timestamp
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    new_photo_path = os.path.join("User_data", f"img_{timestamp}.jpg")
+    os.rename(photo_path, new_photo_path)
+
+
+
+
+
+
+updater = Updater("yourcode")
+dispatcher = updater.dispatcher
+
+dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CommandHandler("help", help_))
+
+dispatcher.add_handler(MessageHandler(Filters.text, message))
+
+dispatcher.add_handler(MessageHandler(Filters.photo, image))
+
+
+updater.start_polling()
+updater.idle()
